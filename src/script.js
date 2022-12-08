dcb791dcf85d8f23114cbb71ec777f125362d826
LiubovSheina <113945968+LiubovSheina@users.noreply.github.com>
2022-12-08 15:15:51 +0200

    forecast

--- src/script.js
+++ src/script.js
@@ -1,34 +1,4 @@
-/* let cities = [
-  {city : 'paris',
-    temp: 20,
-    humility: 20,
-  },
-  {city : 'kyiv',
-    temp: 10,
-    humility: 10,
-  },
-  {city : 'london',
-    temp: 10,
-    humility: 90,
-  },
-  {city : 'lisobon',
-    temp: 30,
-    humility: 40,
-  }
-];
-let answer = prompt("Enter city");
-let p = false;
-for(let i=0;i < cities.length; i++){
- 
-        if (answer.toLowerCase() === cities[i].city) {
-          alert(`It is currently ${cities[i].temp}°C in ${cities[i].city.toUpperCase()} with a humidity of ${cities[i].humility}%`);
-       p = true;
-        };
-      
-      };
-if(p === false){
-        alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${answer.toLowerCase()}`);
-      };*/
+
 
 let now = new Date();
 let listDay = [
@@ -50,6 +20,7 @@ let listMonth = [
   "July",
   "August",
   "September",
+  "October",
   "November",
   "December"
 ];
@@ -61,4 +32,5 @@ let h2 = document.querySelector(".currently-date");
 let hour = now.getHours();
 let min = now.getMinutes();
 h2.innerHTML = `${day} ${hour}:${min},<br/> ${month} ${days}, ${year}.`;
-
+console.log(month);
+console.log(now.getMonth());
